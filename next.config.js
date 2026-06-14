de37bb0c762d75a7d97fb351b10878f3e043c1d4
Benoren80 <benoren80@gmail.com>
2026-06-14 10:29:27 -0500

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['p16-sign.tiktokcdn-us.com', 'p19-sign.tiktokcdn-us.com', 'p16-sign-va.tiktokcdn.com', 'cdninstagram.com', 'scontent.cdninstagram.com'],
-  },
-}
-module.exports = nextConfig
